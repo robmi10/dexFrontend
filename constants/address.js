906db21aed4b536e48c2379b42518169777b4011
robel.michael <robel.michael@onelab.se>
2022-12-22 07:55:48 +0100

    contracts

---
+++ constants/address.js
@@ -0,0 +1,5 @@
+export const DaiToken = "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82";
+
+export const LiquidityToken = "0x9A676e781A523b5d0C0e43731313A708CB607508";
+
+export const Dex = "0x0B306BF915C4d645ff596e518fAf3F9669b97016";
